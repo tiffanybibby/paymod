010ce8149a27f7705f9b9bbbe079664a619e7a60
Tiffany Bibby <88211804+tiffanybibby@users.noreply.github.com>
2025-10-26 00:34:02 -0400

    modified package; Added OpenAPI dependency and a `/actuator/health` for credibility - basic app running

---
+++ src/main/java/com/tiffany/paymod/Main.java
@@ -0,0 +1,12 @@
+package com.tiffany.paymod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.print("Hello World!\n");
+    }
+}
