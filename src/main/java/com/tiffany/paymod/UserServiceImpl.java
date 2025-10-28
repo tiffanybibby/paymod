87a62fcc47aa6a45c3f5fb0922420f1e2901d61a
Tiffany Bibby <88211804+tiffanybibby@users.noreply.github.com>
2025-10-28 18:06:38 -0400

    added @Override to prevent accidental overloads

--- src/main/java/com/tiffany/paymod/UserServiceImpl.java
+++ src/main/java/com/tiffany/paymod/UserServiceImpl.java
@@ -12,19 +12,23 @@ public class UserServiceImpl implements UserService {
     private final List<User> userList = new ArrayList<>();
     private Long nextId = 1L;
 
+    @Override
     public List<User> fetchAllUsers() {
         return userList;
     }
 
+    @Override
     public Optional<User> fetchUser(Long id) {
         return userList.stream().filter(user -> user.getId().equals(id)).findFirst();
     }
 
+    @Override
     public void addUser(User user) {
         user.setId(nextId++);
         userList.add(user);
     }
 
+    @Override
     public boolean updateUser(Long id, User updatedUser) {
         return userList.stream()
                 .filter(user -> user.getId().equals(id)).findFirst()
@@ -35,6 +39,7 @@ public class UserServiceImpl implements UserService {
                 .orElse(false);
     }
 
+    @Override
     public boolean deleteUser(Long id) {
         return userList.removeIf(user -> user.getId().equals(id));
     }
