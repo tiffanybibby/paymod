1b722cbf1d259ce7e352110609949313b62efdfa
Tiffany Bibby <88211804+tiffanybibby@users.noreply.github.com>
2025-10-27 23:33:06 -0400

    implemented intial user model, controller, and service - CRUD working

---
+++ src/main/java/com/tiffany/paymod/User.java
@@ -0,0 +1,10 @@
+package com.tiffany.paymod;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long id;
+    private String email;
+}
---
+++ src/main/java/com/tiffany/paymod/UserController.java
@@ -0,0 +1,52 @@
+package com.tiffany.paymod;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.fetchUser(id)
+                .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        userService.addUser(user);
+        return ResponseEntity.ok("User added successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        boolean updated = userService.updateUser(id, updatedUser);
+        if (updated) {
+            return ResponseEntity.ok("User updated successfully");
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        boolean deleted = userService.deleteUser(id);
+        if (deleted) {
+            return ResponseEntity.ok("User deleted successfully");
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
