4a0f4ff35710bee85698620d91e5c0b46b221f87
Tiffany Bibby <88211804+tiffanybibby@users.noreply.github.com>
2025-11-01 19:37:00 -0400

    jpa user table naming cleanup

--- src/main/java/com/tiffany/paymod/model/User.java
+++ src/main/java/com/tiffany/paymod/model/User.java
@@ -1,13 +1,11 @@
 package com.tiffany.paymod.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
-@Entity(name = "user_table")
+@Entity
+@Table(name="user_table")
 public class User {
 
     @Id
