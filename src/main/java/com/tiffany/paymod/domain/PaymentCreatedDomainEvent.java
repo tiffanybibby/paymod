7c7263da316810ee5e684ea4db3f5fb54599a44c
Tiffany Bibby <88211804+tiffanybibby@users.noreply.github.com>
2025-11-01 22:18:31 -0400

    updates to PaymentServiceImpl to add @transactional annotation to create and update methods and aftercommit event handling

---
+++ src/main/java/com/tiffany/paymod/domain/PaymentCreatedDomainEvent.java
@@ -0,0 +1,4 @@
+package com.tiffany.paymod.domain;
+
+public record PaymentCreatedDomainEvent(Long paymentId)  {
+}
---
+++ src/main/java/com/tiffany/paymod/domain/PaymentStatusChangedDomainEvent.java
@@ -0,0 +1,4 @@
+package com.tiffany.paymod.domain;
+
+public record PaymentStatusChangedDomainEvent(Long paymentId)  {
+}
