013ad63e91c2fe13d2e67833060c50b9378b5664
Tiffany Bibby <88211804+tiffanybibby@users.noreply.github.com>
2025-11-02 17:07:20 -0500

    fixing formatting for vite config

--- ui/paymod-ui/vite.config.js
+++ ui/paymod-ui/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-    proxy: { "/api": { target: "http://localhost:8080", changeOrigin: true } },
+        proxy: { "/api": { target: "http://localhost:8080", changeOrigin: true } },
 },
 })
