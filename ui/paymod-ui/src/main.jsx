4ee35411fd76df0691a3ef2c8642e7c17411dd37
Tiffany Bibby <88211804+tiffanybibby@users.noreply.github.com>
2025-11-02 17:09:34 -0500

    fixed app screen layout

--- ui/paymod-ui/src/main.jsx
+++ ui/paymod-ui/src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import {SaltProvider} from "@salt-ds/core";
 import "@salt-ds/theme/index.css";
+import { ModuleRegistry, AllCommunityModule } from "ag-grid-community";
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <SaltProvider mode="light" density="high">
+      <SaltProvider mode="light" density="medium">
     <App />
       </SaltProvider>
   </StrictMode>,
