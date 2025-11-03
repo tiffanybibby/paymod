6eac43acf9ba47930542afcdd7d514a616f6cb21
Tiffany Bibby <88211804+tiffanybibby@users.noreply.github.com>
2025-11-03 01:26:09 -0500

    added support for light and dark mode - user preference

--- ui/paymod-ui/src/main.jsx
+++ ui/paymod-ui/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import {StrictMode, useEffect, useState} from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,10 +7,42 @@ import "@salt-ds/theme/index.css";
 import { ModuleRegistry, AllCommunityModule } from "ag-grid-community";
 ModuleRegistry.registerModules([AllCommunityModule]);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <SaltProvider mode="light" density="medium">
+function usePrefersColorScheme() {
+    const query = "(prefers-color-scheme: dark)";
+    const getMode = () =>
+        window.matchMedia?.(query).matches ? "dark" : "light";
+
+    const [mode, setMode] = useState(getMode);
+
+    useEffect(() => {
+        const mq = window.matchMedia(query);
+        const onChange = (e) => setMode(e.matches ? "dark" : "light");
+
+        if (mq.addEventListener) {
+            mq.addEventListener("change", onChange);
+            return () => mq.removeEventListener("change", onChange);
+        }
+        // Fallback for very old Safari
+        mq.addListener?.(onChange);
+        return () => mq.removeListener?.(onChange);
+    }, []);
+
+    console.log(mode)
+    return mode;
+}
+
+function Root() {
+    const mode = usePrefersColorScheme();
+return (
+      <SaltProvider mode={mode} density="medium">
     <App />
       </SaltProvider>
-  </StrictMode>,
-)
+);
+}
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Root />
+    </StrictMode>
+);
+
